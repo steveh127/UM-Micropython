df44ef48e850a8e82ad646c30096887492fbf66b
Steve Hardy <steve-hardy@hotmail.com>
2023-12-16 07:13:28 +0000

    unexpected maker boards MP files

---
+++ Bling/bling.py
@@ -0,0 +1,157 @@
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep
+
+class Colour():
+	def __init__(self,pattern,brightness):
+		self.pattern=pattern
+		self.brightness=brightness
+	
+	def __call__(self,brightness=None):
+		if brightness is None:
+			brightness = self.brightness
+		colour=[0,0,0]
+		for i in range(3):
+			if self.pattern[i]:
+				colour[i]=int(brightness*self.pattern[i])
+		return colour
+                    
+class Bling(NeoPixel):
+	chars={
+	' ':('0','0','0','0','0','0'),
+	'.':('0','0','0','0','0','1'),
+	'!':('1','1','1','1','0','1'),
+	',':('0','0','0','0','0','1','1'),
+	':':('0','0','1','0','1','0'),
+	';':('0','0','1','0','1','1'),
+	'?':('110','001','001','010','000','010'),
+	
+	'A':('00100','01010','10001','11111','10001','10001'),
+	'B':('1110','1001','1110','1001','1001','1110'),
+	'C':('0111','1000','1000','1000','1000','0111'),
+	'D':('1110','1001','1001','1001','1001','1110'),
+	'E':('1111','1000','1110','1000','1000','1111'),
+	'F':('1111','1000','1110','1000','1000','1000'),
+	'G':('0111','1000','1000','1011','1001','0110'),		
+	'H':('10001','10001','11111','10001','10001','10001'),
+	'I':('111','010','010','010','010','111'),
+	'J':('011','001','001','001','001','110'),
+	
+	'R':('1110','1001','1001','1110','1010','1001'),
+	
+	'T':('11111','00100','00100','00100','00100','00100'),
+	
+	'a':('0000','0110','0001','0111','1001','0111'),
+	'b':('1000','1000','1110','1001','1001','1110'),
+	'c':('000','011','100','100','100','011'),
+	'd':('0001','0001','0111','1001','1001','0111'),
+	'e':('0000','1110','1001','1111','1000','0111'),
+	'f':('0011','0100','1110','0100','0100','0100'),
+	'g':('0000','0111','1001','1001','0111','0001','0110'),
+	'h':('1000','1000','1110','1001','1001','1001'),
+	'i':('00','10','00','10','10','01'),
+	'j':('001','000','001','001','001','001','110'),
+	
+	'r':('0000','0000','1011','1100','1000','1000'),
+	
+	't':('0000','0100','1111','0100','0100','0011'),	
+	
+	'K':(	
+	'1001',
+	'1010',
+	'1100',
+	'1100',
+	'1010',
+	'1001'
+	),
+	'k':(	
+	'000',
+	'100',
+	'100',
+	'101',
+	'110',
+	'101'
+	),
+			
+	}
+	
+	
+	def __init__(self):
+		NeoPixel.__init__(self,Pin(18),320)
+		self.power=Pin(6,Pin.OUT)
+		self.power.on()
+		self.brightness=5
+		
+		self.RED = Colour((1,0,0),self.brightness)
+		self.GREEN = Colour((0,1,0),self.brightness)
+		self.YELLOW = Colour((2,1,0),self.brightness)
+		self.ORANGE = Colour((4,0.5,0),self.brightness)
+		self.BLUE = Colour((0,0,1),self.brightness)
+		self.PURPLE = Colour((3,0,1),self.brightness)
+		self.WHITE = Colour((1,1,1),self.brightness)
+		self.BLACK = Colour((0,0,0),self.brightness)
+		
+		self.background=self.BLACK(0)
+		self.colour=self.GREEN()
+		
+		self.lines=[0,40,80,120,160,200,240,280]
+
+	def col(self,n,colour=None):
+		if colour is None:
+			colour=self.colour
+		for line in self.lines:
+			self[line+n] = colour
+			self.write()
+
+	def row(self,n,colour=None):
+		if colour is None:
+			colour=self.colour
+		for i in range(40):
+			self[self.lines[n]+i] = colour
+		self.write()
+		
+	def show_char(self,char,col,colour=None):
+		if colour is None:
+			colour=self.colour
+		character=self.chars[char]
+		width = len(character[0])
+		line=1
+		for row in character:
+			p=0
+			for c in row:
+				if c == '1':
+					self[self.lines[line]+col+p]=colour
+				p += 1
+			line += 1
+		self.write()
+		return width
+	
+	def show_text(self,text,colour=None,justify='C'):
+		lt = 0
+		for c in text:
+			lt += len(self.chars[c][0]) + 1
+		lt -= 1
+		if lt > 38:
+			#scroll or cut
+			self.show_text('TTTTTT')
+			return
+		if justify == 'C' or justify == 'R':
+			if justify == 'C':
+				column = (39 - lt)//2
+			if justify == 'R':
+				column = 39 - lt
+		else:
+			column=1
+		if colour is None:
+			colour=self.colour
+		for ch in text:
+			column += self.show_char(ch,column,colour) + 1
+		
+d=Bling()
+d.background=(d.BLUE(1))
+d.fill(d.background)
+d.show_text('Thief',colour=d.RED(),justify='L')
+d.show_text('Kk?',justify='R')
+	
+
+	
