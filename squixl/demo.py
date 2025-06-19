f911674290cabb740d2c54cec3ae95033c62cd5c
Steve Hardy <steve-hardy@hotmail.com>
2025-06-19 09:01:36 +0100

    button improved

--- squixl/demo.py
+++ squixl/demo.py
@@ -5,27 +5,44 @@ from squixl_time import SQ_Time
 from squixl_touch import SQ_Touch
 from fonts import *
 
-def print_hit():
+async def print_hit():
 	print('Touched')
-
-def print_h2():
+	await asyncio.sleep(0.5)
+	
+async def print_h2():
 	print('Corner Touched')
-
-def pressed():
+	await asyncio.sleep(0.5)
+	
+async def pressed():
 	print('Pressed')
-
-def test():
+	await asyncio.sleep(0.5)
+	
+async def test():
 	print('Test')
-
-def button(text,x,y,colour,*,font,action=None):
-	scr = get_screen()	
-	w,h = scr.get_size(text,font)
-	b=int(h//6)
-	scr.rect(x-b,y-b,w + (2 * b) + 2,h + (2 * b))
-	scr.write_over(text,x,y,colour,font=font)
-	SQ_Touch().add_target((x,y,w,h),action)
-
-
+	await asyncio.sleep(0.5)
+	
+class button():
+	def __init__(self,text,x,y,colour,*,font,clicked,action=None):
+		self.text = text
+		self.action = action
+		self.colour = colour
+		self.clicked = clicked
+		self.font = font
+		self.x = x
+		self.y = y
+		self.scr = get_screen()	
+		self.w,self.h = self.scr.get_size(text,font)
+		self.b=int(self.h//4)
+		self.scr.rect(self.x - self.b,self.y-self.b,self.w + (2 * self.b) + 2,self.h + (2 * self.b))
+		self.scr.write_over(self.text,self.x,self.y,self.colour,font=self.font,background=self.clicked)
+		SQ_Touch().add_target((self.x,self.y,self.w,self.h),self.do_action)
+	
+	async def do_action(self):
+		self.scr.write_over(self.text,self.x,self.y,self.clicked,font=self.font,background=self.colour)
+		await self.action()
+		await asyncio.sleep(0.5)
+		self.scr.write_over(self.text,self.x,self.y,self.colour,font=self.font,background=self.clicked)
+	
 async def main():
 	scr = get_screen()
 	scr.font = sans24I
@@ -34,8 +51,8 @@ async def main():
 	scr.write("Testing 1 2 3", 200, 200, GREEN,rotation=45)
 	scr.write("Going, going, gone...",100, 300, BLUE,rotation=-30,font=serif32B)
 	
-	button("Micropython is now running SQUiXL",80, 400,WHITE,font=sans24I,action=pressed)
-	button("TEST",200, 20,RED,font=sans24B,action=test)
+	button("Micropython is now running SQUiXL",80, 400,WHITE,clicked=RED,font=sans24I,action=pressed)
+	button("TEST",200, 20,RED,clicked=GREEN,font=sans24B,action=test)
 	
 	clock = SQ_Time(350,10,GREEN,font=sans32)
 	i = 1
@@ -47,9 +64,9 @@ async def main():
 
 	scr.write_over(' ' + str(0) + ' ',10, 10, YELLOW,background=RED)
 	while True:
-		if t.touched:
-			t.touched = False
-			print(t.point)
+		# if t.touched:
+			# t.touched = False
+			# print(t.point)
 		if i % 10 == 0:
 			scr.write_over(' ' + str(i//10) + ' ',10, 10, YELLOW,background=RED)
 		await asyncio.sleep(1)
--- squixl/squixl_touch.py
+++ squixl/squixl_touch.py
@@ -3,11 +3,10 @@ import asyncio
 from SQUiXL import touch
 
 class SQ_Touch():
-	targets = []
 	
 	def __init__(self):
+		self.targets = []
 		self.touch = touch
-		self.touched = False
 		self.point = -1,-1
 		asyncio.create_task(self.check())
 		
@@ -20,17 +19,17 @@ class SQ_Touch():
 		last_y = last_x = -1
 		while True:
 			points = self.touch.read_points()[1]
-			if last_y != points[0][0] and last_x != points[0][1]:
+			if (last_y != points[0][0]) and (last_x != points[0][1]):
 				point = points[0][1],points[0][0]
-				self.check_targets(point)
+				await self.check_targets(point)
 				last_x = points[0][1]
 				last_y = points[0][0]
-			await asyncio.sleep(0.1)
+			await asyncio.sleep(0.3)
 	
-	def check_targets(self,point):
+	async def check_targets(self,point):
 		for target in self.targets:
 			x,y = point
 			x_min,x_max,y_min,y_max = target[0]
 			if (x > x_min) and (x < x_max) and (y > y_min) and (y < y_max):
-				target[1]()
+				await target[1]()
 				break
